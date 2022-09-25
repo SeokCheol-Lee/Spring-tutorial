986d3c6f497988747d3fb38b49719b8444482831
Lee SeokCheol <90691610+SeokCheol-Lee@users.noreply.github.com>
2022-09-25 15:35:40 +0900

    second commit

--- src/main/java/hellospring/hellospring/service/MemberService.java
+++ src/main/java/hellospring/hellospring/service/MemberService.java
@@ -2,14 +2,18 @@ package hellospring.hellospring.service;
 
 import hellospring.hellospring.domain.Member;
 import hellospring.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
 
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
